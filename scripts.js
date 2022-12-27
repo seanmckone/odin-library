992f60b90ad77979132b4daf1c95f1d60b330bf9
SDMckone <seandmckone@gmail.com>
2022-12-27 13:20:08 -0500

    Add function to display books

--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
+const content = document.querySelector(".content");
+
 const myLibrary = [];
 
 function Book(title, author, pageCount, beenRead) {
@@ -28,3 +30,45 @@ function Book(title, author, pageCount, beenRead) {
 function addBookToLibrary(book) {
     myLibrary.push(book);
 }
+
+function displayBooks(library) {
+    for (let i = 0; i < library.length; i += 1) {
+        const bookcard = document.createElement("div");
+        bookcard.classList.toggle("bookcard");
+
+        const div = document.createElement("div");
+
+        const title = document.createElement("p");
+        title.classList.toggle("title");
+        title.textContent = `${library[i].title}`;
+
+        const authorname = document.createElement("p");
+        authorname.classList.toggle("author");
+        authorname.textContent = `${library[i].author}`;
+
+        div.appendChild(title);
+        const br = document.createElement("br");
+        div.appendChild(br);
+        div.appendChild(authorname);
+
+        bookcard.appendChild(div);
+
+        const pagecount = document.createElement("p");
+        pagecount.classList.toggle("pagecount");
+        pagecount.textContent = `Pages: ${library[i].pageCount}`;
+
+        bookcard.appendChild(pagecount);
+
+        content.appendChild(bookcard);
+    }
+}
+
+const book1 = new Book("Book 1", "Author 1", 234, false);
+const book2 = new Book("Book 2", "Author 2", 345, true);
+const book3 = new Book("Book 3", "Author 3", 2313, false);
+
+addBookToLibrary(book1);
+addBookToLibrary(book2);
+addBookToLibrary(book3);
+
+displayBooks(myLibrary);
