db0261f7a7c6a4885252e32e91269d9dca4e6135
SDMckone <seandmckone@gmail.com>
2022-12-30 13:53:50 -0500

    Add deletion functionality

--- scripts.js
+++ scripts.js
@@ -2,6 +2,7 @@ const bookList = document.querySelector(".booklist");
 const addBook = document.querySelector(".addbook");
 const content = document.querySelector(".content");
 const addbutton = document.querySelector(".addbutton");
+const closeaddbook = document.querySelector(".closeaddbook");
 const addBookForm = document.getElementById("addbook_form");
 
 addBook.style.display = "none";
@@ -14,6 +15,7 @@ function Book(title, author, pageCount, beenRead, coverColor) {
     this.pageCount = pageCount;
     this.beenRead = beenRead;
     this.coverColor = coverColor;
+    this.toDisplay = true;
 
     this.info = () => {
         let returnString = "";
@@ -34,45 +36,57 @@ function Book(title, author, pageCount, beenRead, coverColor) {
     };
 }
 
-function addBookToLibrary(book) {
-    myLibrary.push(book);
-}
-
 function displayBooks(library) {
     while (bookList.hasChildNodes()) {
         bookList.removeChild(bookList.lastChild);
     }
 
     for (let i = 0; i < library.length; i += 1) {
-        const bookcard = document.createElement("div");
-        bookcard.style.backgroundColor = library[i].coverColor;
-        bookcard.classList.toggle("bookcard");
+        if (library[i].toDisplay) {
+            const bookcard = document.createElement("div");
+            bookcard.style.backgroundColor = library[i].coverColor;
+            bookcard.classList.toggle("bookcard");
 
-        const div = document.createElement("div");
+            const div = document.createElement("div");
 
-        const title = document.createElement("p");
-        title.classList.toggle("title");
-        title.textContent = `${library[i].title}`;
+            const title = document.createElement("p");
+            title.classList.toggle("title");
+            title.textContent = `${library[i].title}`;
 
-        const authorname = document.createElement("p");
-        authorname.classList.toggle("author");
-        authorname.textContent = `${library[i].author}`;
+            const authorname = document.createElement("p");
+            authorname.classList.toggle("author");
+            authorname.textContent = `${library[i].author}`;
 
-        const pagecount = document.createElement("p");
-        pagecount.classList.toggle("pagecount");
-        pagecount.textContent = `Pages: ${library[i].pageCount}`;
+            const pagecount = document.createElement("p");
+            pagecount.classList.toggle("pagecount");
+            pagecount.textContent = `Pages: ${library[i].pageCount}`;
 
-        const br = document.createElement("br");
+            const br = document.createElement("br");
 
-        div.appendChild(title);
-        div.appendChild(br);
-        div.appendChild(authorname);
+            div.appendChild(title);
+            div.appendChild(br);
+            div.appendChild(authorname);
 
-        bookcard.appendChild(div);
-        bookcard.appendChild(pagecount);
+            bookcard.appendChild(div);
+            bookcard.appendChild(pagecount);
 
-        bookList.appendChild(bookcard);
+            const trashbutton = document.createElement("button");
+            trashbutton.classList.toggle("trashbutton");
+            trashbutton.textContent = "t";
+            trashbutton.addEventListener("click", () => {
+                myLibrary[i].toDisplay = false;
+                bookcard.style.display = "none";
+            });
+            bookcard.appendChild(trashbutton);
+
+            bookList.appendChild(bookcard);
+        }
     }
+}
+
+function addBookToLibrary(book) {
+    myLibrary.push(book);
+    displayBooks(myLibrary);
 }
 
 addBookForm.addEventListener("submit", (event) => {
@@ -95,24 +109,7 @@ addbutton.addEventListener("click", () => {
     addBook.style.display = "block";
 });
 
-// const book1 = new Book("Book 1", "Author 1", 234, false);
-// const book2 = new Book("Book 2", "Author 2", 345, true);
-// const book3 = new Book("Book 3", "Author 3", 2313, false);
-
-// addBookToLibrary(book1);
-// addBookToLibrary(book2);
-// addBookToLibrary(book3);
-// addBookToLibrary(book1);
-// addBookToLibrary(book2);
-// addBookToLibrary(book3);
-// addBookToLibrary(book1);
-// addBookToLibrary(book2);
-// addBookToLibrary(book3);
-// addBookToLibrary(book1);
-// addBookToLibrary(book2);
-// addBookToLibrary(book3);
-// addBookToLibrary(book1);
-// addBookToLibrary(book2);
-// addBookToLibrary(book3);
-
-displayBooks(myLibrary);
+closeaddbook.addEventListener("click", () => {
+    content.style.opacity = "100%";
+    addBook.style.display = "none";
+});
